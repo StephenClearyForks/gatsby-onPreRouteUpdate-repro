6ecca06434f01352dd8712df4f712e9abac1d676
Stephen Cleary <github.ourteddybear@xoxy.net>
2021-03-26 21:09:57 -0400

    Add repro.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,7 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.onPreRouteUpdate = ({ location, prevLocation }) => {
+    console.log("Gatsby started to change location from", prevLocation, "to", location);
+};
